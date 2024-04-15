030014bb08dbb082caf03c5b087404268898302e
raipen <raipendalk@gmail.com>
2024-04-15 21:51:00 +0900

    feat: set global prefix and vlidatoin pipe

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import {
 } from '@nestjs/platform-fastify';
 import { swagger } from './swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
   swagger(app);
   await app.listen(3000);
 }
